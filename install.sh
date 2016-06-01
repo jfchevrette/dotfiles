bc564de0e44d092b66df668a7f0f5cbb7ca8a186
Jean-Francois Chevrette <jf.cron0@gmail.com>
2016-06-01 13:41:53 -0400

    add font

--- install.sh
+++ install.sh
@@ -62,4 +62,4 @@ brew cask install $CASKAPPS 2> /dev/null
 
 # Install fonts
 brew tap caskroom/fonts
-brew cask install font-source-code-pro
+brew cask install font-source-code-pro font-sauce-code-powerline
