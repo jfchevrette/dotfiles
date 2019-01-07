e587e73d36fe14ba05c0f3660e6d1c393b3b54ff
Jean-Francois Chevrette <jfchevrette@gmail.com>
2019-01-07 10:16:58 -0500

    polybar: playerctl should show if media player is not available

--- polybar/.config/polybar/playerctl.sh
+++ polybar/.config/polybar/playerctl.sh
@@ -6,7 +6,13 @@ if ! hash playerctl; then
 fi
 
 STATUS=$(playerctl status 2> /dev/null)
+RC=$?
+if [[ $RC -eq 1 ]]; then
+  echo -n "Media player not available"
+  exit
+fi
+
 ARTIST=$(playerctl metadata xesam:artist 2> /dev/null)
 TITLE=$(playerctl metadata xesam:title 2> /dev/null)
 
-echo "%{F#FFF}[$STATUS] $ARTIST - $TITLE%{F- B- -o}"
+echo -n "%{F#FFF}[$STATUS] $ARTIST - $TITLE%{F- B- -o}"
