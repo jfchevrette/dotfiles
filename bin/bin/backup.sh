ce301861bf6d1ec185d81433f3248f87769481d1
Jean-Francois Chevrette <jfchevrette@gmail.com>
2020-04-03 12:28:34 -0400

    bin: update backup script

--- bin/bin/backup.sh
+++ bin/bin/backup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-sudo borg create -v --progress --stats \
+sudo borg create --umask 0027 -v --progress --stats \
   --exclude $HOME/.cache \
   --exclude $HOME/.gem \
+  --exclude $HOME/.local/share/containers \
   --exclude $HOME/.rustup \
   --exclude $HOME/.zgen \
   --exclude $HOME/Downloads \
   /mnt/backups/archy::$(date +%d-%m-%Y_%H:%M:%S) \
   /etc $HOME
 
+sudo borg prune --umask 0027 -v /mnt/backups/archy
+  --keep-hourly 24 \
+  --keep-daily 7 \
+  --keep-weekly 4 \
+  --keep-monthly 6
