eaff05325905fabbe4103dcb7329e58dbc58b7aa
Jean-Francois Chevrette <jfchevrette@gmail.com>
2020-06-09 16:44:33 -0400

    bin: update backup script

--- bin/bin/backup.sh
+++ bin/bin/backup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-sudo borg create --umask 0027 -v --progress --stats \
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root."
+  exit 1
+fi
+
+read -sp 'Borg passphrase: ' BORG_PASSPHRASE
+export BORG_PASSPHRASE
+
+echo "Creating backup"
+borg create --umask 0027 -v --progress --stats \
   --exclude $HOME/.cache \
   --exclude $HOME/.gem \
   --exclude $HOME/.local/share/containers \
@@ -10,7 +19,8 @@ sudo borg create --umask 0027 -v --progress --stats \
   /mnt/backups/archy::$(date +%d-%m-%Y_%H:%M:%S) \
   /etc $HOME
 
-sudo borg prune --umask 0027 -v /mnt/backups/archy
+echo "Pruning backups"
+borg prune --umask 0027 -v /mnt/backups/archy \
   --keep-hourly 24 \
   --keep-daily 7 \
   --keep-weekly 4 \
