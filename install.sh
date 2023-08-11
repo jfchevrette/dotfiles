10d8705b3a233c2316e1b27388f9834241d6a7cc
Jean-Francois Chevrette <jfchevrette@gmail.com>
2023-08-10 21:21:29 -0400

    cleanup stuff I don't use anymore

--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-awk -F: '{print $1}' index | xargs ./stow.sh
--- nvim/.config/nvim/lua/todel/plugins/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local ok, _ = pcall(require, "lspconfig")
-if not ok then
-  return
-end
-
-require "user.plugins.lsp.lsp-installer"
-require("user.plugins.lsp.handlers").setup()
-require "user.plugins.lsp.null-ls"
